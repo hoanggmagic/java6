b33b076cd14c2a33b1375d6521bbf56f7319e07a
hadtpd09887 <hadtpd09887@gmail.com>
2025-03-17 19:54:12 +0700

    java6

--- src/main/java/com/example/java6/Controller/HomeController.java
+++ src/main/java/com/example/java6/Controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.example.java6.Controller;
+package com.example.java6.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
